aacd9ed08583116dcc280c7772aac468e68304b4
朱凯宾 <912549963@qq.com>
2019-10-22 20:19:44 +0800

    添加项目文件。

---
+++ ProConsoleDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ProConsoleDemo
+{
+    class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Hello World!");
+        //}
+
+        static async Task Main(string[] args)
+        {
+            var client = new HttpClient();
+            var result = await client.GetStringAsync("https://www.baidu.com/");
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
